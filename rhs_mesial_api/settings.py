6fc9ea2955d4d0753efd0ebcb1c26a8b8b4a3d65
Gabriel Hogan <Gabriel.T.Hogan@gmail.com>
2021-09-26 10:53:49 -0400

    Fix cors brokeness

--- rhs_mesial_api/settings.py
+++ rhs_mesial_api/settings.py
@@ -25,6 +25,9 @@ SECRET_KEY = 'django-insecure-m^w&_&ye2^*15y=yvy)yy$&^heu6#4v^7es5bh5e^^772c4k(z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# CORS_ORIGIN_ALLOW_ALL = DEBUG
+
+
 ALLOWED_HOSTS = []
 
 
@@ -57,12 +60,33 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8080',
+    'http://127.0.0.1:8080',
+    'http://*',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'cache-control',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 
 ROOT_URLCONF = 'rhs_mesial_api.urls'
 
