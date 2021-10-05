af47fd5c8ef6cc6316a130cd5b9ed7983962cc50
Samuel Rembisz <sjrembisz07@gmail.com>
2021-10-05 15:18:58 -0400

    Fixed timezone settings issues in Django

--- apps/schedule/schedule.py
+++ apps/schedule/schedule.py
@@ -1,7 +1,6 @@
 from __future__ import print_function
 
 import datetime, os
-import pytz
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 from django.utils import timezone
